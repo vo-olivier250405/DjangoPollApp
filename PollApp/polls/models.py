b9ca40d4273d85c8083f59a8359306526eb4adda
vo_o <vo_o@etna-alternance.net>
2024-03-10 13:56:39 +0100

    [ADD] => adding admin page

--- PollApp/polls/models.py
+++ PollApp/polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
 # Create your models here.
 
@@ -12,6 +14,14 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     release_date = models.DateTimeField("date published")
 
+    def __str__(self):
+        """_summary_
+        """
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.release_date >= timezone.now() - timedelta(days=1)
+
 
 class Choice(models.Model):
     """_summary_
@@ -22,3 +32,8 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        """_summary_
+        """
+        return self.choice_text
