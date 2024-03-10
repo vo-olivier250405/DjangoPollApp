2813beefd88e6bbcfcbfcb7e12c58445f06d784d
vo_o <vo_o@etna-alternance.net>
2024-03-10 15:04:45 +0100

    [ADD] => adding the vote option

--- PollApp/polls/views.py
+++ PollApp/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from django.template import loader
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.db.models import F
+from django.urls import reverse
 from .models import Question, Choice
 
 
@@ -31,9 +32,21 @@ def results(request, question_id):
         request (_type_): _description_
         question_id (_type_): _description_
     """
-    response = f"You're looking at the result of question {question_id}"
-    return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        "question": question,
+        "error_message": "You didn't select a choice.",
+    },
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", context)
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
